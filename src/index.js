75ea6fd8bea216c9c404a65dcf2b802457f262f6
German <terbiyarchitect@gmail.com>
2019-10-18 21:34:36 +0300

    Create an interface for the required commands
    Fix ESLint rule setting.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,53 @@ const net = require('net')
 const { host, port } = require('../config.json')
 
 const server = net.createServer(connection => {
-  console.log('Client connected')
+  connection.setEncoding('utf8')
 
-  connection.on('end', () => {
-    console.log('client disconnected')
+  connection.on('data', query => {
+    const response = handleIncommingQuery(query)
+
+    console.log(response)
+    connection.write(response)
+    connection.write('\r\n')
   })
-
-  connection.write('Hello\r\n')
 
   connection.pipe(connection)
 })
 
 server.listen(port, host)
+
+function handleIncommingQuery(query) {
+  const command = parseCommand(query)
+
+  const handle =
+    {
+      create,
+      insert,
+      delete: deleteDocument,
+      get
+    }[command] || (() => `No command ${command} found.`)
+
+  return handle()
+}
+
+function create(query) {
+  return 'creating'
+}
+
+function insert(query) {
+  return 'inserting'
+}
+
+function deleteDocument(query) {
+  return 'deleting'
+}
+
+function get(query) {
+  return 'getting'
+}
+
+function parseCommand(query) {
+  const [command = ''] = query.split(':')
+
+  return command
+}
