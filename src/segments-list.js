397ac4a9a6806b4b80f65688ef74e19c1af041fe
German <terbiyarchitect@gmail.com>
2019-10-22 22:48:55 +0300

    Add the list of segments to handle upcoming formatting

---
+++ src/segments-list.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const { Segment } = require('./segment.js')
+
+class SegmentsList {
+  constructor() {
+    this.list = null
+  }
+
+  addSegment(start, end) {
+    const segment = new Segment(start, end)
+
+    if (!this.list) {
+      this.list = segment
+    } else {
+      this.insertSegment(segment)
+    }
+
+    return this
+  }
+
+  insertSegment(segment) {
+    let previousSegment = null
+    let currentSegment = this.list
+    let segmentToInsert = segment
+
+    do {
+      if (currentSegment.followsAfterDistantly(segmentToInsert)) {
+        segmentToInsert.setNext(currentSegment)
+
+        if (previousSegment === null) {
+          this.list = segmentToInsert
+        } else {
+          previousSegment.setNext(segmentToInsert)
+        }
+
+        return
+      }
+
+      if (currentSegment.followsAfterImmediatly(segmentToInsert)) {
+        currentSegment.merge(segmentToInsert)
+
+        return
+      }
+
+      if (
+        currentSegment.intersects(segmentToInsert) ||
+        currentSegment.staysImmediatlyBefore(segmentToInsert)
+      ) {
+        currentSegment.merge(segmentToInsert)
+
+        if (segmentToInsert.next) {
+          currentSegment.setNext(segmentToInsert.next)
+        }
+
+        if (!currentSegment.next || segmentToInsert === currentSegment.next) {
+          currentSegment.setNext(null)
+
+          return
+        }
+
+        segmentToInsert = currentSegment.next
+
+        continue
+      }
+
+      if (currentSegment.next) {
+        previousSegment = currentSegment
+        currentSegment = currentSegment.next
+
+        continue
+      }
+
+      if (!currentSegment.next) {
+        currentSegment.setNext(segmentToInsert)
+
+        return
+      }
+    } while (true)
+  }
+}
+
+module.exports = {
+  SegmentsList
+}
